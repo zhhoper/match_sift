604f59b48220c66e4d1d42d0c1546affe09e7bea
zhhoper <zhhoper@gmail.com>
2015-12-08 17:29:10 -0500

    load testing pair names from folders

---
+++ load_files.m
@@ -0,0 +1,17 @@
+function pair_list = load_files(folder_name)
+% this function is used to load testing pairs making use of all the images
+
+pair_list = {};
+count = 1;
+file_names = dir(strcat(folder_name, '*.jpg'));
+
+num_files = length(file_names);
+
+for i = 1 : num_files - 1
+		for j = i+1 : num_files
+				pair_list{count, 1} = file_names(i).name;
+				pair_list{count, 2} = file_names(j).name;
+				count = count + 1;
+		end
+end
+end
